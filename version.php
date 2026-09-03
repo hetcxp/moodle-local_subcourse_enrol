16dffa1b4c1eb6f00921aafca5ef5b1e5827eb66
hetcxp <hetcxp@gmail.com>
2026-09-03 09:42:15 -0300

    chore: set required Moodle version to 4.5 (2024100700)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_subcourseenrol';
-$plugin->version   = 2026082603;
-$plugin->requires  = 2026041300; // Moodle 5.0+
+$plugin->version   = 2026090300;
+$plugin->requires  = 2024100700; // Moodle 4.5+
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '2.0.0';
 $plugin->dependencies = [
